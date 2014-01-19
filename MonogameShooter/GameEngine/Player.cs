27f8b4438ca18014304bf713fdf7f1ac41b28186
ForsakenSwat <vshevch@gmail.com>
2014-01-19 17:36:21 +0400

    player

---
+++ MonogameShooter/GameEngine/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonogameShooter.GameEngine
+{
+
+
+    class Player
+    {
+
+        public int HP, SP, Level;
+        public string Name;
+        public Texture2D Portrait;
+
+        public DrawPortrait(Texture2D Portrait)
+        {
+            
+        }
+    }
+}
